b8b1712f38378a5a7a162217d1e66e4f8f77dbd7
Thales Ferri Juliani <thales.juliani@gmail.com>
2021-10-04 20:49:22 -0300

    Novo Trabalho

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Trabalho Final Lets Code - Lista de Presença Escola.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Borá Criar
         </a>
       </header>
     </div>
