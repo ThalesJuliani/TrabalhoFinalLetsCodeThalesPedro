dacbad1d48c80e114fdaee3c7049acbe594dccbd
Thales Ferri Juliani <thales.juliani@gmail.com>
2021-10-25 19:08:05 -0300

    7

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ const [curso, setAge] = useState ('3')
       <p className='titulo2'> Invista em seu Futuro </p>  
       <p className='titulo3'> Educação de Qualidade </p>  
       <p className='Unidade'>  <TextInput value={nome} onChangeValue={setName} /> </p>
-      
-      <p className='tituloCurso'> Escolha: 1- Curso Drenagem Linfática / 2 - Botox / 3 - Curso Pós-Operatório </p>  
+
+      <p className='tituloCurso'> Escolha: 1- Curso Drenagem Linfática / 2 - Botox </p>  
       <p className='nomecurso'> <Input type="text" value={value} onChangeValue={setValue} /> </p>
       <p className='escolhecurso'> <CursoEscolher curso = {value} /> </p>
 
--- src/Components/CursoEscolher/cursoescolher.js
+++ src/Components/CursoEscolher/cursoescolher.js
@@ -1,16 +1,15 @@
 export function CursoEscolher({curso}) {
-    if ( curso = 1) {
+    if ( curso <= 1) {
         return <p> Curso de Drenagem Lifática - Dia 31/10 - Domingo</p>      
-   } 
-   else if ( curso = 2) {
-        return <p> Curso de Botox - Dia 30/10 - Sábado</p>
-   } 
-   else if ( curso = 3) 
-   {
-     return <p> Curso Pós-Operatório -  Dia 29/10 - Sexta-Feira</p>
-   } 
-   else  {
+      } 
+      else if ( curso >= 1) 
+      {
+      return <p> Curso de Botox - Dia 30/10 - Sabado</p>
+      }
+          
+      else  
+     {
      return <p> Curso Dia - Aguardando Informação</p>
-   } 
+     } 
        
 }
