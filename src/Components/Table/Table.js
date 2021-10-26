7e61b06e457e76b3500c750e24cdbe207fbb4fec
Thales Ferri Juliani <thales.juliani@gmail.com>
2021-10-26 19:58:23 -0300

    9

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,4 +1,4 @@
-function Table({lista, colunaNome, width = 'auto', high} = 'auto') {
+function Table({list, colNames, width = 'auto', high} = 'auto') {
     return(
         <div>
             {lista.length > 0 && (
